83b0fbd21a862805eef9cdc24d20ce2dd4c74626
Vinge Nikolay <vn2756@gmail.com>
2021-09-09 20:37:54 +0400

    RBTreeSerializer

---
+++ SimpleDB.Test/DataStructures/RBTreeSerializerTest.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using SimpleDB.DataStructures;
+using SimpleDB.Test.Tools;
+
+namespace SimpleDB.Test.DataStructures
+{
+    class RBTreeSerializerTest
+    {
+        private MemoryFileStream _stream;
+        private RBTree<int, int> _tree;
+
+        [SetUp]
+        public void Setup()
+        {
+            _stream = new MemoryFileStream();
+            _tree = new RBTree<int, int>();
+        }
+
+        [Test]
+        public void SerializeDeserialize_EmptyTree()
+        {
+            RBTreeSerializer.Serialize(_tree, _stream);
+            _stream.Seek(0, System.IO.SeekOrigin.Begin);
+            var result = RBTreeSerializer.Deserialize<int, int>(_stream);
+            Assert.AreEqual(null, result.Root);
+        }
+
+        [Test]
+        public void SerializeDeserialize_One()
+        {
+            _tree.Insert(new RBTree<int, int>.Node(1) { Value = 1 });
+            RBTreeSerializer.Serialize(_tree, _stream);
+            _stream.Seek(0, System.IO.SeekOrigin.Begin);
+            var result = RBTreeSerializer.Deserialize<int, int>(_stream);
+            var originalNodes = _tree.Root.GetAllChildren();
+            var resultNodes = result.Root.GetAllChildren();
+            TreesAreEquals(originalNodes, resultNodes);
+        }
+
+        [Test]
+        public void SerializeDeserialize_Two()
+        {
+            _tree.Insert(new RBTree<int, int>.Node(1) { Value = 1 });
+            _tree.Insert(new RBTree<int, int>.Node(2) { Value = 2 });
+            RBTreeSerializer.Serialize(_tree, _stream);
+            _stream.Seek(0, System.IO.SeekOrigin.Begin);
+            var result = RBTreeSerializer.Deserialize<int, int>(_stream);
+            var originalNodes = _tree.Root.GetAllChildren();
+            var resultNodes = result.Root.GetAllChildren();
+            TreesAreEquals(originalNodes, resultNodes);
+        }
+
+        [Test]
+        public void SerializeDeserialize_Three()
+        {
+            _tree.Insert(new RBTree<int, int>.Node(1) { Value = 1 });
+            _tree.Insert(new RBTree<int, int>.Node(2) { Value = 2 });
+            _tree.Insert(new RBTree<int, int>.Node(3) { Value = 3 });
+            RBTreeSerializer.Serialize(_tree, _stream);
+            _stream.Seek(0, System.IO.SeekOrigin.Begin);
+            var result = RBTreeSerializer.Deserialize<int, int>(_stream);
+            var originalNodes = _tree.Root.GetAllChildren();
+            var resultNodes = result.Root.GetAllChildren();
+            TreesAreEquals(originalNodes, resultNodes);
+        }
+
+        [Test]
+        public void SerializeDeserialize_100()
+        {
+            for (int i = 0; i < 100; i++) _tree.Insert(new RBTree<int, int>.Node(i) { Value = i });
+            RBTreeSerializer.Serialize(_tree, _stream);
+            _stream.Seek(0, System.IO.SeekOrigin.Begin);
+            var result = RBTreeSerializer.Deserialize<int, int>(_stream);
+            var originalNodes = _tree.Root.GetAllChildren();
+            var resultNodes = result.Root.GetAllChildren();
+            TreesAreEquals(originalNodes, resultNodes);
+        }
+
+        [Test]
+        public void SerializeDeserialize_1000()
+        {
+            for (int i = 0; i < 1000; i++) _tree.Insert(new RBTree<int, int>.Node(i) { Value = i });
+            RBTreeSerializer.Serialize(_tree, _stream);
+            _stream.Seek(0, System.IO.SeekOrigin.Begin);
+            var result = RBTreeSerializer.Deserialize<int, int>(_stream);
+            var originalNodes = _tree.Root.GetAllChildren();
+            var resultNodes = result.Root.GetAllChildren();
+            TreesAreEquals(originalNodes, resultNodes);
+        }
+
+        [Test]
+        public void SerializeDeserialize_KeyString_1000()
+        {
+            var tree = new RBTree<string, string>();
+            for (int i = 0; i < 1000; i++) tree.Insert(new RBTree<string, string>.Node(i.ToString()) { Value = i.ToString() });
+            RBTreeSerializer.Serialize(tree, _stream);
+            _stream.Seek(0, System.IO.SeekOrigin.Begin);
+            var result = RBTreeSerializer.Deserialize<string, string>(_stream);
+            var originalNodes = tree.Root.GetAllChildren();
+            var resultNodes = result.Root.GetAllChildren();
+            TreesAreEquals(originalNodes, resultNodes);
+        }
+
+        class TestKey : IComparable<TestKey>
+        {
+            public string Value { get; set; }
+
+            public int CompareTo([AllowNull] TestKey x)
+            {
+                return Value.CompareTo(x.Value);
+            }
+        }
+
+        [Test]
+        public void SerializeDeserialize_KeyObject_1000()
+        {
+            var tree = new RBTree<TestKey, string>();
+            for (int i = 0; i < 1000; i++) tree.Insert(new RBTree<TestKey, string>.Node(new TestKey { Value = i.ToString() }) { Value = i.ToString() });
+            RBTreeSerializer.Serialize(tree, _stream);
+            _stream.Seek(0, System.IO.SeekOrigin.Begin);
+            var result = RBTreeSerializer.Deserialize<TestKey, string>(_stream);
+            var originalNodes = tree.Root.GetAllChildren();
+            var resultNodes = result.Root.GetAllChildren();
+            TreesAreEquals(originalNodes, resultNodes);
+        }
+
+        private void TreesAreEquals(List<RBTree<int, int>.Node> originalNodes, List<RBTree<int, int>.Node> resultNodes)
+        {
+            Assert.AreEqual(originalNodes.Count, resultNodes.Count);
+            for (int i = 0; i < originalNodes.Count; i++)
+            {
+                Assert.AreEqual(originalNodes[i].Key, resultNodes[i].Key);
+                Assert.AreEqual(originalNodes[i].Color, resultNodes[i].Color);
+                Assert.AreEqual(originalNodes[i].Value, resultNodes[i].Value);
+                if (originalNodes[i].Left != null)
+                {
+                    Assert.AreEqual(originalNodes[i].Left.Key, resultNodes[i].Left.Key);
+                }
+                else
+                {
+                    Assert.AreEqual(null, resultNodes[i].Left);
+                }
+                if (originalNodes[i].Right != null)
+                {
+                    Assert.AreEqual(originalNodes[i].Right.Key, resultNodes[i].Right.Key);
+                }
+                else
+                {
+                    Assert.AreEqual(null, resultNodes[i].Right);
+                }
+            }
+        }
+
+        private void TreesAreEquals(List<RBTree<string, string>.Node> originalNodes, List<RBTree<string, string>.Node> resultNodes)
+        {
+            Assert.AreEqual(originalNodes.Count, resultNodes.Count);
+            for (int i = 0; i < originalNodes.Count; i++)
+            {
+                Assert.AreEqual(originalNodes[i].Key, resultNodes[i].Key);
+                Assert.AreEqual(originalNodes[i].Color, resultNodes[i].Color);
+                Assert.AreEqual(originalNodes[i].Value, resultNodes[i].Value);
+                if (originalNodes[i].Left != null)
+                {
+                    Assert.AreEqual(originalNodes[i].Left.Key, resultNodes[i].Left.Key);
+                }
+                else
+                {
+                    Assert.AreEqual(null, resultNodes[i].Left);
+                }
+                if (originalNodes[i].Right != null)
+                {
+                    Assert.AreEqual(originalNodes[i].Right.Key, resultNodes[i].Right.Key);
+                }
+                else
+                {
+                    Assert.AreEqual(null, resultNodes[i].Right);
+                }
+            }
+        }
+
+        private void TreesAreEquals(List<RBTree<TestKey, string>.Node> originalNodes, List<RBTree<TestKey, string>.Node> resultNodes)
+        {
+            Assert.AreEqual(originalNodes.Count, resultNodes.Count);
+            for (int i = 0; i < originalNodes.Count; i++)
+            {
+                Assert.AreEqual(originalNodes[i].Key.Value, resultNodes[i].Key.Value);
+                Assert.AreEqual(originalNodes[i].Color, resultNodes[i].Color);
+                Assert.AreEqual(originalNodes[i].Value, resultNodes[i].Value);
+                if (originalNodes[i].Left != null)
+                {
+                    Assert.AreEqual(originalNodes[i].Left.Key.Value, resultNodes[i].Left.Key.Value);
+                }
+                else
+                {
+                    Assert.AreEqual(null, resultNodes[i].Left);
+                }
+                if (originalNodes[i].Right != null)
+                {
+                    Assert.AreEqual(originalNodes[i].Right.Key.Value, resultNodes[i].Right.Key.Value);
+                }
+                else
+                {
+                    Assert.AreEqual(null, resultNodes[i].Right);
+                }
+            }
+        }
+    }
+}
---
+++ SimpleDB/DataStructures/RBTreeSerializer.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Text;
+using SimpleDB.Infrastructure;
+
+namespace SimpleDB.DataStructures
+{
+    internal static class RBTreeSerializer
+    {
+        private static readonly byte _emptyTreeFlag = 255;
+
+        public static void Serialize<TKey, TValue>(RBTree<TKey, TValue> tree, IWriteableStream stream) where TKey : IComparable<TKey>
+        {
+            if (tree.Root == null) stream.WriteByte(_emptyTreeFlag);
+            else WriteNodeRec(tree.Root, stream);
+        }
+
+        private static void WriteNodeRec<TKey, TValue>(RBTree<TKey, TValue>.Node node, IWriteableStream stream) where TKey : IComparable<TKey>
+        {
+            WriteNode(node, stream);
+            if (node.Left != null) WriteNodeRec(node.Left, stream);
+            if (node.Right != null) WriteNodeRec(node.Right, stream);
+        }
+
+        private static void WriteNode<TKey, TValue>(RBTree<TKey, TValue>.Node node, IWriteableStream stream) where TKey : IComparable<TKey>
+        {
+            byte flags = node.Color == RBTree<TKey, TValue>.Color.Red ? (byte)0 : (byte)1;
+            flags |= node.Left != null ? (byte)2 : (byte)0;
+            flags |= node.Right != null ? (byte)4 : (byte)0;
+            stream.WriteByte(flags);
+            WriteNodeKey(node.Key, stream);
+            var valueBytes = BinarySerialization.ToBinary(node.Value);
+            stream.WriteInt(valueBytes.Length);
+            stream.WriteByteArray(valueBytes, 0, valueBytes.Length);
+        }
+
+        private static void WriteNodeKey(object nodeKey, IWriteableStream stream)
+        {
+            var nodeKeyType = nodeKey.GetType();
+            if (nodeKeyType == typeof(bool))
+            {
+                stream.WriteBool((bool)nodeKey);
+            }
+            else if (nodeKeyType == typeof(sbyte))
+            {
+                stream.WriteSByte((sbyte)nodeKey);
+            }
+            else if (nodeKeyType == typeof(byte))
+            {
+                stream.WriteByte((byte)nodeKey);
+            }
+            else if (nodeKeyType == typeof(char))
+            {
+                stream.WriteChar((char)nodeKey);
+            }
+            else if (nodeKeyType == typeof(short))
+            {
+                stream.WriteShort((short)nodeKey);
+            }
+            else if (nodeKeyType == typeof(ushort))
+            {
+                stream.WriteUShort((ushort)nodeKey);
+            }
+            else if (nodeKeyType == typeof(int))
+            {
+                stream.WriteInt((int)nodeKey);
+            }
+            else if (nodeKeyType == typeof(uint))
+            {
+                stream.WriteUInt((uint)nodeKey);
+            }
+            else if (nodeKeyType == typeof(long))
+            {
+                stream.WriteLong((long)nodeKey);
+            }
+            else if (nodeKeyType == typeof(ulong))
+            {
+                stream.WriteULong((ulong)nodeKey);
+            }
+            else if (nodeKeyType == typeof(float))
+            {
+                stream.WriteFloat((float)nodeKey);
+            }
+            else if (nodeKeyType == typeof(double))
+            {
+                stream.WriteDouble((double)nodeKey);
+            }
+            else if (nodeKeyType == typeof(decimal))
+            {
+                stream.WriteDecimal((decimal)nodeKey);
+            }
+            else if (nodeKeyType == typeof(DateTime))
+            {
+                stream.WriteLong(((DateTime)nodeKey).ToBinary());
+            }
+            else if (nodeKeyType == typeof(string))
+            {
+                var bytes = Encoding.UTF8.GetBytes((string)nodeKey);
+                stream.WriteInt(bytes.Length);
+                stream.WriteByteArray(bytes, 0, bytes.Length);
+            }
+            else
+            {
+                var fieldValueJson = JsonSerialization.ToJson(nodeKey);
+                var bytes = Encoding.UTF8.GetBytes(fieldValueJson);
+                stream.WriteInt(bytes.Length);
+                stream.WriteByteArray(bytes, 0, bytes.Length);
+            }
+        }
+
+        public static RBTree<TKey, TValue> Deserialize<TKey, TValue>(IReadableStream stream) where TKey : IComparable<TKey>
+        {
+            var emptyFlag = stream.ReadByte();
+            if (emptyFlag == _emptyTreeFlag) return new RBTree<TKey, TValue>();
+            else stream.Seek(-1, System.IO.SeekOrigin.Current);
+            bool hasLeft, hasRight;
+            var root = ReadNode<TKey, TValue>(stream, out hasLeft, out hasRight);
+            if (hasLeft) ReadNodeRec(root, true, stream);
+            if (hasRight) ReadNodeRec(root, false, stream);
+
+            return new RBTree<TKey, TValue>(root);
+        }
+
+        private static void ReadNodeRec<TKey, TValue>(RBTree<TKey, TValue>.Node parent, bool toLeft, IReadableStream stream) where TKey : IComparable<TKey>
+        {
+            bool hasLeft, hasRight;
+            var node = ReadNode<TKey, TValue>(stream, out hasLeft, out hasRight);
+            if (toLeft)
+            {
+                parent.Left = node;
+                node.Parent = parent;
+            }
+            else
+            {
+                parent.Right = node;
+                node.Parent = parent;
+            }
+            if (hasLeft) ReadNodeRec(node, true, stream);
+            if (hasRight) ReadNodeRec(node, false, stream);
+        }
+
+        private static RBTree<TKey, TValue>.Node ReadNode<TKey, TValue>(IReadableStream stream, out bool hasLeft, out bool hasRight) where TKey : IComparable<TKey>
+        {
+            var flags = stream.ReadByte();
+            var color = (flags & 1) == 0 ? RBTree<TKey, TValue>.Color.Red : RBTree<TKey, TValue>.Color.Black;
+            hasLeft = (flags & 2) > 0;
+            hasRight = (flags & 4) > 0;
+            var key = (TKey)ReadNodeKey<TKey>(stream);
+            var valueLength = stream.ReadInt();
+            var value = BinarySerialization.FromBinary<TValue>(stream.ReadByteArray(valueLength));
+
+            return new RBTree<TKey, TValue>.Node(key) { Color = color, Value = value };
+        }
+
+        private static object ReadNodeKey<TKey>(IReadableStream stream)
+        {
+            var nodeKeyType = typeof(TKey);
+            if (nodeKeyType == typeof(bool))
+            {
+                return stream.ReadBool();
+            }
+            else if (nodeKeyType == typeof(sbyte))
+            {
+                return stream.ReadSByte();
+            }
+            else if (nodeKeyType == typeof(byte))
+            {
+                return stream.ReadByte();
+            }
+            else if (nodeKeyType == typeof(char))
+            {
+                return stream.ReadChar();
+            }
+            else if (nodeKeyType == typeof(short))
+            {
+                return stream.ReadShort();
+            }
+            else if (nodeKeyType == typeof(ushort))
+            {
+                return stream.ReadUShort();
+            }
+            else if (nodeKeyType == typeof(int))
+            {
+                return stream.ReadInt();
+            }
+            else if (nodeKeyType == typeof(uint))
+            {
+                return stream.ReadUInt();
+            }
+            else if (nodeKeyType == typeof(long))
+            {
+                return stream.ReadLong();
+            }
+            else if (nodeKeyType == typeof(ulong))
+            {
+                return stream.ReadULong();
+            }
+            else if (nodeKeyType == typeof(float))
+            {
+                return stream.ReadFloat();
+            }
+            else if (nodeKeyType == typeof(double))
+            {
+                return stream.ReadDouble();
+            }
+            else if (nodeKeyType == typeof(decimal))
+            {
+                return stream.ReadDecimal();
+            }
+            else if (nodeKeyType == typeof(DateTime))
+            {
+                return DateTime.FromBinary(stream.ReadLong());
+            }
+            else if (nodeKeyType == typeof(string))
+            {
+                var length = stream.ReadInt();
+                var bytes = stream.ReadByteArray(length);
+                return Encoding.UTF8.GetString(bytes);
+            }
+            else
+            {
+                var length = stream.ReadInt();
+                var bytes = stream.ReadByteArray(length);
+                var fieldValueJson = Encoding.UTF8.GetString(bytes);
+                return JsonSerialization.FromJson(typeof(TKey), fieldValueJson);
+            }
+        }
+    }
+}
